017745e9ed52c3cb482d9a3ac132c5c83ecbf0cb
ChinweCodes <njokuchinwendum@gmail.com>
2022-07-11 16:22:21 -0600

    Quzzies completed

---
+++ loops/findMySeat.js
@@ -0,0 +1,42 @@
+/**
+ * Creative Commons - Jeffrey Smith - "State Theater"
+
+Theater seats often display a row and seat number to help theatergoers find their seats. If there are 26 rows (0 to 25) and 100 seats (0 to 99) in each row, write a nested for loop to print out all of the different seat combinations in the theater.
+
+Example output for row-seat information: output each row and seat number on a separate line
+
+0-0
+0-1
+0-2
+...
+25-97
+25-98
+25-99
+ */
+
+/*
+ * Programming Quiz: Find my Seat (4-8)
+ * 
+ * Write a nested for loop to print out all of the different seat combinations in the theater.
+ * The first row-seat combination should be 0-0 
+ * The last row-seat combination will be 25-99
+ * 
+ * Things to note: 
+ *  - the row and seat numbers start at 0, not 1
+ *  - the highest seat number is 99, not 100
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should use a nested `for` loop'
+ * - Your code should produce the expected output, as explained above
+ */
+ 
+// Write your code here
+for(var x = 0; x <= 25; x++)
+{
+    for(var y = 0; y < 100; y++)
+    {
+        console.log(x+"-"+y);
+    }
+}
